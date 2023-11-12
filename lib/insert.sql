6d9d6dec6d744bf4adb1b09ef2c2b911660155ca
Connor <connordbrown@gmail.com>
2023-11-11 20:24:09 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 5, "M", "Brown", "Good", 1),
+("Rowdy", 6, "M", "Black", "Bad", 0),
+("Tabitha", "7", "F", "Brown", "Good", 1),
+("Sergeant Brown", 10, "M", "Gray", "Bad", 1),
+("Melissa", 2, "F", "Black", "Good", 0),
+("Grinch", 9, "M", "Brown", "Bad", 1),
+("Wendy", 12, "F", "Black", "Bad", 1),
+(Null, NULL, "F", "Gray", NULL, 1);
